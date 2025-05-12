701c48620820f846ce482b8f8dbc071a35141983
DucReal <Vietduc9596@gmail.com>
2025-05-12 19:52:57 +0700

    init

---
+++ src/test/java/com/ducdv38/springsecurity/SpringsecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ducdv38.springsecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringsecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
