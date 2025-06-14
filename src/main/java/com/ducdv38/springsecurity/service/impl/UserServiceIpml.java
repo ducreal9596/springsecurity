d90d6690b1ad6b6286b89b84015618e9bd146136
DucReal <Vietduc9596@gmail.com>
2025-06-14 21:17:02 +0700

    t

--- src/main/java/com/ducdv38/springsecurity/service/impl/UserServiceIpml.java
+++ src/main/java/com/ducdv38/springsecurity/service/impl/UserServiceIpml.java
@@ -75,7 +75,7 @@ public class UserServiceIpml implements IUserService {
           // (END) 2025-06-14 K23-840 DEV DucDV38 ADD Category10
         User user = userRepository.findByUsername(name).orElseThrow(() -> new AppException(ErrorCode.USER_NOT_EXIST));
                  // (STR) 2025-06-14 K23-840 DEV DucDV38 MOD Category10
-        return userMapper.toUserResponse(user);
+        // return userMapper.toUserResponse(user);
                  // (STR) 2025-06-14 K23-840 DEV DucDV38 MOD Category10
     }
 
